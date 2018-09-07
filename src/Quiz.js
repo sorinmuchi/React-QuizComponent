f6de36c09e0099b61c59532cf5478b2fbb24ccb9
Sorin Muchi <sorin.muchi@mail.mcgill.ca>
2018-09-07 14:37:18 -0700

    Finished Module 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import QuizEnd from './QuizEnd.js'
 import QuizQuestion from './QuizQuestion.js'
 
 let quizData = require('./quiz_data.json');
@@ -10,7 +11,11 @@ class Quiz extends Component {
     }
 
     render() {
-        return (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>);
+        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+
+        return (<div> {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+            </div>
+        );
     }
 }
 
